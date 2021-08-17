bb4e5baa3dd5509a1a4da0165345a7be5dd440bf
Khoa <khoahc98@gmail.com>
2021-08-17 11:12:47 -0400

    fix port

--- app.js
+++ app.js
@@ -12,7 +12,7 @@ const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
-const port = server.listen(process.env.PORT || 3000);
+const port = process.env.PORT || 3000;
 app.set("view engine", "ejs");
 
 app.use(express.urlencoded({ extended: true }));
